0f53dc311780b69f5c0650027b9378e99ca484f9
Ruth Naebeck <ruthnaebeck@gmail.com>
2018-08-03 18:23:16 -0400

    Can copy / paste

--- popup.js
+++ popup.js
@@ -1,12 +1,14 @@
-"use strict";
-/* global chrome */
+'use strict';
+/* global chrome slackRawText */
+
+var slackRawText = '';
 
 function getCurrentTabUrl(callback) {
   var queryInfo = {
     active: true,
     currentWindow: true
   };
-  chrome.tabs.query(queryInfo, function(tabs) {
+  chrome.tabs.query(queryInfo, function (tabs) {
     var tab = tabs[0];
     var url = tab.url;
     console.assert(typeof url == 'string', 'tab.url should be a string');
@@ -14,24 +16,50 @@ function getCurrentTabUrl(callback) {
   });
 }
 
+function slackCopy() {
+  chrome.tabs.executeScript(null,
+    {code: `
+      chrome.storage.local.set({ 'slackRawText':  window.getSelection().toString()})
+      chrome.storage.local.get('slackRawText', function (result) {
+        console.log(result.slackRawText)
+      });
+    `});
+  setTimeout(function(){
+    window.close();
+  }, 2000);
+}
+
 function zendeskPaste() {
-  var x = document.getElementsByClassName('editor zendesk-editor--rich-text-comment')[0];
-  var node = document.createElement('p');
-  var textnode = document.createTextNode('Added with code');
-  node.appendChild(textnode);
-  x.appendChild(node);
-  console.log('Zendesk Paste Complete!')
+  chrome.tabs.executeScript(null,
+    {code: `
+      chrome.storage.local.get('slackRawText', function (result) {
+        console.log(result.slackRawText)
+        var x = document.getElementsByClassName('editor zendesk-editor--rich-text-comment')[0];
+        var node = document.createElement('p');
+        var textnode = document.createTextNode(result.slackRawText);
+        node.appendChild(textnode);
+        x.appendChild(node);
+        console.log('Zendesk Paste Complete!')
+      });
+    `});
+  setTimeout(function(){
+    window.close();
+  }, 2000);
 }
 
 function renderHTML(value) {
   document.getElementById('slackZendesk').innerHTML = value;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    if(url.indexOf('datadog.zendesk.com/agent/tickets') > -1){
+document.addEventListener('DOMContentLoaded', function () {
+  getCurrentTabUrl (function (url) {
+    if (url.indexOf('dd.slack.com') > -1) {
+      slackCopy()
+      renderHTML('Copied')
+    } else if (url.indexOf('datadog.zendesk.com/agent/tickets') > -1) {
+      zendeskPaste()
       renderHTML('Pasted');
-    }else{
+    } else {
       renderHTML('Page Not Supported');
     }
   });
