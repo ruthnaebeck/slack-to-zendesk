ab1d437223a6afd8d5bfa76946e1d4dd14a2331a
Ruth Naebeck <ruthnaebeck@gmail.com>
2018-08-03 21:04:52 -0400

    Separated by new line + trimmed

--- popup.js
+++ popup.js
@@ -1,8 +1,6 @@
 'use strict';
 /* global chrome slackRawText */
 
-var slackRawText = '';
-
 function getCurrentTabUrl(callback) {
   var queryInfo = {
     active: true,
@@ -17,13 +15,20 @@ function getCurrentTabUrl(callback) {
 }
 
 function slackCopy() {
-  var reg = /\[.*ago\]/g
+  var regTime = /\[.*ago\]/g;
+  var newLine = /\r?\n/;
   chrome.tabs.executeScript(null,
     {code: `
-      var slackParsedText = window.getSelection().toString();
-      slackParsedText = slackParsedText.replace(${reg}, '<br>')
-      chrome.storage.local.set({ 'slackParsedText':  slackParsedText})
-      console.log(slackParsedText)
+      var slackParse = window.getSelection().toString().split(${newLine}).map(item => {return item.trim()})
+      var i = 0;
+      while (i < slackParse.length) {
+        if (${regTime}.test(slackParse[i]))
+          slackParse.splice(i, 1);
+        else
+          i++;
+      }
+      chrome.storage.local.set({'slackParse': slackParse})
+      console.log(slackParse)
     `});
   setTimeout(function(){
     window.close();
@@ -33,11 +38,11 @@ function slackCopy() {
 function zendeskPaste() {
   chrome.tabs.executeScript(null,
     {code: `
-      chrome.storage.local.get('slackParsedText', function (result) {
-        console.log(result.slackParsedText)
+      chrome.storage.local.get('slackParse', function (result) {
+        console.log(result.slackParse)
         var x = document.getElementsByClassName('editor zendesk-editor--rich-text-comment')[0];
         var node = document.createElement('p');
-        var textnode = document.createTextNode(result.slackParsedText);
+        var textnode = document.createTextNode(result.slackParse);
         node.appendChild(textnode);
         x.appendChild(node);
         console.log('Zendesk Paste Complete!')
